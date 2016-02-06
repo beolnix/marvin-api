22def277b4b7eb949d971c6566902e70bbc018a6
beolnix <beolnix@gmail.com>
2016-02-06 14:20:55 +0100

    simplify PluginConfig

--- src/main/java/com/beolnix/marvin/plugins/api/PluginConfig.java
+++ src/main/java/com/beolnix/marvin/plugins/api/PluginConfig.java
@@ -12,31 +12,19 @@ import java.util.logging.Logger;
  * Created by beolnix on 06/02/16.
  */
 public class PluginConfig {
-    private final String logsDirPath;
     private final Logger logger;
-    private final String pluginDirPath;
     private final File pluginDir;
     private final Map<String, String> propMap = new HashMap<>();
 
-    public PluginConfig(String logsDirPath, Logger logger, String pluginDirPath, File pluginDir, List<Property> properties) {
-        this.logsDirPath = logsDirPath;
+    public PluginConfig(Logger logger, File pluginDir, List<Property> properties) {
         this.logger = logger;
-        this.pluginDirPath = pluginDirPath;
         this.pluginDir = pluginDir;
 
         properties.forEach(p -> propMap.put(p.getName(), p.getValue()));
     }
 
-    public String getLogsDirPath() {
-        return logsDirPath;
-    }
-
     public Logger getLogger() {
         return logger;
-    }
-
-    public String getPluginDirPath() {
-        return pluginDirPath;
     }
 
     public File getPluginDir() {
